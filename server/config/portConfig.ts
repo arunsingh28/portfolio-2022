66d15e9aa7183d2a926758249af55c41cec831e5
Arun Singh <arunsingh28aug.as@gmail.com>
2021-11-23 01:03:58 +0530

    update

---
+++ server/config/portConfig.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
+const SERVER_PORT = process.env.SERVER_POPT || 4000;
+
+const config = {
+  hostname: SERVER_HOSTNAME,
+  port: SERVER_PORT,
+};
+
+export default config;
