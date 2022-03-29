c60ee753ce4f4a256c673b8f57aea0fc3245876d
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-03-29 22:40:39 +0530

    change workflow

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Person from '../../assets/person.png'
+import PersonImg from '../../assets/person.png'
 const Navbar = () => {
   
   return (
     <div className="flex justify-between font-sans">
       <div>brand</div>
       <div>menu</div>
-      <div>7983613144</div>
-      <img src={Person} alt="person" />
+      <div>7</div>
+      <img src={PersonImg} alt="person" />
     </div>
   );
 };
