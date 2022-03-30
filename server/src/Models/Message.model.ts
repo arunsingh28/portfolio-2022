a13882f2f0f2d7d273ea8cb6c315d32158bdc287
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-03-30 15:26:53 +0530

    message schema interface

--- server/src/Models/Message.model.ts
+++ server/src/Models/Message.model.ts
@@ -1,4 +1,12 @@
-import mongoose from 'mongoose'
+import mongoose,{ ObjectId } from 'mongoose'
+
+export interface ImessageSchema{
+    id: ObjectId
+    email : string
+    message: string
+    createdAt: string
+    name: string
+}
 
 const messageSchema = new mongoose.Schema({
     email:{
@@ -10,5 +18,19 @@ const messageSchema = new mongoose.Schema({
             message: 'Please provide valid email'
         },
         required: [true, 'Email required']
-    }
+    },
+    message:{
+        type: String,
+        required: [true, 'Message required']
+    },
+    createdAt: {
+        type : String,
+        default : Date
+    },
+    name: {
+        type: String,
+        required: [true, 'Name required']
+    },
 })
+
+export default mongoose.model<ImessageSchema>('Message',messageSchema)
