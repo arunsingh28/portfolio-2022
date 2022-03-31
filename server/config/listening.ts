7c1712c9b9f8a5cc55e2363075574cc838a26da1
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-03-31 14:47:38 +0530

    database effi v.2

--- server/config/listening.ts
+++ server/config/listening.ts
@@ -2,23 +2,33 @@ import config from "./portConfig";
 import { Express } from "express";
 import mongoose from 'mongoose'
 
-const uri = 'mongodb+srv://arun:1234@cluster0.t3qon.mongodb.net/login-microservice?retryWrites=true&w=majority';
+
 
 const Listening = async (app: Express) => {
   app.listen(config.port, () => {
     console.log(`Server is UP on PORT:${config.port}`);
   });
+
   function dbConnection(){
-    mongoose.connect(uri).then(()=>{
-      console.log('database is connected')
+   try {
+    mongoose.connect(config.uri).then(()=>{
+      console.log('Production database is connected')
     }).catch((e)=>{
-      console.log('database not connected')
+      mongoose.connect('mongodb://localhost:27107/portfolio-query').then(()=>{
+        console.log('Local Database is connected')
+      }).catch((e)=>{
+        console.log('local database is not connected')
+      })
+      console.log('Production database is not connected')
     })
+   } catch (error) {
+     console.log('Both Database server fail to connted')
+   }
     const conSuccess = mongoose.connection;
-    conSuccess.on('error', console.error.bind(console, 'connection error:'));
+    conSuccess.on('error', console.error.bind('connection error:'));
   }
 
-  dbConnection
+  dbConnection()
 };
 
 
--- server/config/portConfig.ts
+++ server/config/portConfig.ts
@@ -2,12 +2,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const uri = 'mongodb+srv://arun:134@cluster0.t3qon.mongodb.net/portfolio-query?retryWrites=true&w=majority';
+
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
 const SERVER_PORT = process.env.SERVER_POPT || 4000;
+const DATABASR_URI = uri
 
 const config = {
   hostname: SERVER_HOSTNAME,
   port: SERVER_PORT,
+  uri: DATABASR_URI
 };
 
 export default config;
